024a14dbe66cdd74fa737c7c09f11929af8c6d39
j <belicja1@gmail.com>
2019-09-04 08:43:06 +0200

    page after jquerry

--- page/script.js
+++ page/script.js
@@ -1,10 +1,8 @@
-const text = document.querySelector(".text");
-const btnShow = document.querySelector(".show");
-const btnHide = document.querySelector(".hide");
-
-btnShow.addEventListener("click", function() {
-    text.style.display = "block";
-});
-btnHide.addEventListener("click", function() {
-    text.style.display = "none";
+$(document).ready(function() {
+    $(".show").click(function() {
+        $(".text").show();
+    });
+    $(".hide").click(function() {
+        $(".text").hide();
+    });
 });
