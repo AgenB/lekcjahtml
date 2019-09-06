704504a3f47fa50df7a3ba9195acd45d7ac0a1ed
j <belicja1@gmail.com>
2019-09-06 11:05:10 +0200

    click boxes

--- page/script.js
+++ page/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-    $(".show").click(function() {
-        $(".text").show();
-    });
-    $(".hide").click(function() {
-        $(".text").hide();
+    for (let x = 0; x < 50; x += 1) {
+        let obj = document.createElement("div");
+        obj.className = "obj";
+        obj.innerHTML = x;
+        $("body").append(obj);
+    }
+    $(".obj").click(function() {
+        $(this).hide();
     });
 });
