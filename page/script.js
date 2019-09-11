c858070b215b835802e145fa6850e8d6d06bb063
j <belicja1@gmail.com>
2019-09-11 09:34:00 +0200

    update

--- page/script.js
+++ page/script.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
     $("p").click(function() {
-        if ($(this).css("background-color") == "rgb(255, 0, 0)") {
-            $(this).css("background-color", "yellow");
-        }
-        else {
-            $(this).css("background-color", "red");
-        }
+        $(this).addClass("yellow");
+        $(this).toggleClass("red");
         $(this).css("font-size", parseInt($(this).css("font-size")) + 1 + "px");
     })
 });
