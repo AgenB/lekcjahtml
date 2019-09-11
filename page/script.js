f27f008cc0527d6830198928d8266db3a95f6276
j <belicja1@gmail.com>
2019-09-11 08:55:00 +0200

    stuff

--- page/script.js
+++ page/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    for (let x = 0; x < 50; x += 1) {
-        let obj = document.createElement("div");
-        obj.className = "obj";
-        obj.innerHTML = x;
-        $("body").append(obj);
-    }
-    $(".obj").click(function() {
-        $(this).hide();
-    });
+    $("p").click(function() {
+        if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+            $(this).css("background-color", "yellow");
+        }
+        else {
+            $(this).css("background-color", "red");
+        }
+        $(this).css("font-size", parseInt($(this).css("font-size")) + 1 + "px");
+    })
 });
