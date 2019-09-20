eadf628a0087619d297a3cb1520ddb8f36bcfeb8
j <belicja1@gmail.com>
2019-09-20 11:24:55 +0200

    update

--- page/script.js
+++ page/script.js
@@ -1,28 +1,11 @@
 $(document).ready(function() {
-    let tableRows = 5;
-    let tableCols = 5;
-    let cellId = 0;
-    let table = document.createElement("table");
-    for (let i = 0; i < tableRows; i++) {
-        let row = document.createElement("tr");
-        for (let y = 0; y < tableCols; y++) {
-            let cell = document.createElement("td");
-            $(cell).attr("cellId", cellId);
-            $(cell).css("border-color", "black");
-            $(cell).mouseenter(function() {
-                $(this).css("border-color", "red");
-            });
-            $(cell).mouseleave(function() {
-                $(this).css("border-color", "black");
-            });
-            $(cell).click(function() {
-                $(this).toggleClass("blue");
-                $("td[cellId='" + $(this).attr("cellId") + "']").toggleClass("blue");
-            });
-            cellId++
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
-    }
-    document.querySelector("div").appendChild(table);
+    $("img").mouseenter(function() {
+        $(this).animate({opacity: "1"}, 10);
+    });
+    $("img").mouseleave(function() {
+        $(this).animate({opacity: "0.5"}, 10);
+    });
+    $("img").click(function() {
+        $(this).nextSibling().fadeToggle();
+    });
 });
