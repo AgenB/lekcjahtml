43619edb85ad55cd851bf500d7deeeee8f7d3e60
j <belicja1@gmail.com>
2019-09-25 09:07:09 +0200

    gonk

--- page/script.js
+++ page/script.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
     $("img").mouseenter(function() {
         $(this).animate({opacity: "1"}, 10);
+        $(this).animate(
+            { top: "200px", left: "200px",deg: 36000 },
+            {
+            duration: 1200,
+            step: function(now) {
+                $(this).css({ transform: 'rotate(' + now + 'deg)' });
+            }
+            }
+        );
     });
     $("img").mouseleave(function() {
         $(this).animate({opacity: "0.5"}, 10);
+        $(this).animate(
+            { top: 0, left: 0, deg: 0 },
+            {
+            duration: 1200,
+            step: function(now) {
+                $(this).css({ transform: 'rotate(' + now + 'deg)' });
+            }
+            }
+        );
     });
     $("img").click(function() {
         $(this).nextSibling().fadeToggle();
