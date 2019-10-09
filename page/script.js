974d70400a05ba26dc5a1cc526c53a7e1ce4acd6
j <belicja1@gmail.com>
2019-10-09 09:00:41 +0200

    ajax in jquery

--- page/script.js
+++ page/script.js
@@ -1,19 +1,7 @@
 $(document).ready(function() {
-    $(document).mousemove(function(e) {
-        $(".parent").css({"left": e.pageX, "top": e.pageY});
-    });
-    $(document).click(function() {
-        $(".xhair").animate({
-            left: "-60px",
-            top: "-60px",
-            height: "120px",
-            width: "120px"
-        });
-        $(".xhair").animate({
-            left: "-50px",
-            top: "-50px",
-            height: "100px",
-            width: "100px"
+    $(".button").click(function() {
+        $.get("test.php", {number: $(".input").val()}, function(data) {
+            $(".result").text(data);
         });
     });
 });
